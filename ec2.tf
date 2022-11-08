309ef211bcbc0ecbfbc314085c33361c16ba6079
FaithOngoro <bfongoro@gmail.com>
2022-11-08 11:05:26 -0700

    adding web server infra

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "dove-instance" {
+    ami = var.AMIS[var.REGION]
+    instance_type          = var.INSTANCE_TYPE
+    availability_zone      = "us-west-2a"
+    key_name               = "jenkins-keypair"
+    vpc_security_group_ids = ["sg-0b3869a314f2d2574"]
+    
+    tags = {
+        "name" = "Airbnb-webserver"
+    }
+}
---
+++ var.tf
@@ -0,0 +1,21 @@
+variable REGION {
+    default = "us-west-2"
+}
+
+variable ZONE1 {
+    default = "us-west-2a"
+}
+
+variable AMIS {
+    type = map
+    default = {
+        us-west-2 = "ami-0d593311db5abb72b"
+        us-west-1 = "ami-017c001a88dd93847"
+    }
+}
+
+variable INSTANCE_TYPE {
+    type = string
+    description = "type of ec2 instance"
+    default = "t2.micro"
+}

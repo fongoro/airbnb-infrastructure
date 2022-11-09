e74604034eb0967f35775675d3848230fa28797b
FaithOngoro <bfongoro@gmail.com>
2022-11-08 18:11:54 -0700

    modified ec2 file

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "dove-instance" {
     ami = var.AMIS[var.REGION]
     instance_type          = var.INSTANCE_TYPE
+    root_block_device {
+      encrypted = true
+    }
+    }
+    monitoring = true
+    ebs_optimized = true
     availability_zone      = "us-west-2a"
     key_name               = "jenkins-keypair"
     vpc_security_group_ids = ["sg-0b3869a314f2d2574"]
